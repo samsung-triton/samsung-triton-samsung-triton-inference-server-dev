ddbba6da80be35ddf881349cd76a5e156f807f33
KDU <54763635+kimdonguk822@users.noreply.github.com>
2025-11-04 09:59:21 +0900

    [S13P31S302-51] Feat: InputLarge 컴포넌트 구현

--- Frontend/triton/lib/widgets/input/input_large.dart
+++ Frontend/triton/lib/widgets/input/input_large.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import '../../theme/app_colors.dart';
 import '../../theme/typography.dart';
+import 'package:flutter/services.dart';
 
-class InputLarge extends StatelessWidget {
+class InputLarge extends StatefulWidget {
   final String hintText;
   final TextEditingController? controller;
   final bool enabled;
   final bool hasError;
   final double width;
-  final double height;
   final double borderRadius;
   final EdgeInsets padding;
-  final VoidCallback? onTap;
+  final int maxLength;
 
   const InputLarge({
     Key? key,
@@ -20,42 +20,65 @@ class InputLarge extends StatelessWidget {
     this.enabled = true,
     this.hasError = false,
     this.width = 309,
-    this.height = 48,
     this.borderRadius = 4,
-    this.padding = const EdgeInsets.symmetric(horizontal: 16),
-    this.onTap,
+    this.padding = const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    this.maxLength = 500,
   }) : super(key: key);
 
+  @override
+  State<InputLarge> createState() => _InputLargeState();
+}
+
+class _InputLargeState extends State<InputLarge> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) _controller.dispose();
+    super.dispose();
+  }
+
   Color _borderColor() {
-    if (!enabled) return lightGray;
-    if (hasError) return statusRed;
+    if (!widget.enabled) return lightGray;
+    if (widget.hasError) return statusRed;
     return lightGray;
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width,
-      height: height,
-      alignment: Alignment.centerLeft,
+      width: widget.width,
+      alignment: Alignment.topLeft,
       decoration: BoxDecoration(
         color: white,
-        borderRadius: BorderRadius.circular(borderRadius),
+        borderRadius: BorderRadius.circular(widget.borderRadius),
         border: Border.all(color: _borderColor(), width: 1),
       ),
-      padding: padding,
+      padding: widget.padding,
       child: TextField(
-        controller: controller,
-        enabled: enabled,
-        onTap: onTap,
-        style: T.t12(color: darkGray, bold: false),
+        controller: _controller,
+        enabled: widget.enabled,
+        keyboardType: TextInputType.multiline,
+        textInputAction: TextInputAction.newline,
+        minLines: 2,
+        maxLines: 5,
+        maxLength: widget.maxLength,
+        maxLengthEnforcement: MaxLengthEnforcement.enforced,
+        style: T.t12(color: darkGray),
         cursorColor: primaryNormal,
         decoration: InputDecoration(
-          isCollapsed: true,
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: T.t12(color: gray),
           border: InputBorder.none,
+          isCollapsed: true,
           contentPadding: EdgeInsets.zero,
+          counterText: '', // 글자수 표시 숨김
         ),
       ),
     );
