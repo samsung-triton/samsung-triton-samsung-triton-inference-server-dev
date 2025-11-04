827fda16e1b78a2fbe7ec0bfa8a29e5f30f322b5
ji_eu_nn <najieun5317@naver.com>
2025-11-04 19:21:56 +0900

    [S13P31S302-49] Feat: router.dart 파일 라우트 경로만 관리하도록 분리

--- Frontend/triton/lib/router.dart
+++ Frontend/triton/lib/router.dart
@@ -7,6 +7,8 @@ import 'package:triton/screen/model_log/model_log_screen.dart';
 import 'package:triton/screen/model_manage/model_manage_screen.dart';
 import 'package:triton/screen/server_log/server_log_screen.dart';
 
+import 'package:triton/app_frame.dart';
+
 // 라우트 경로 관리용 클래스
 class Routes {
   static const login = '/login';
@@ -14,75 +16,6 @@ class Routes {
   static const modelManage = '/model_manage';
   static const modelLog = '/model_log';
   static const serverLog = '/server_log';
-}
-
-// 임시용 헤더 프레임
-class AppFrame extends StatelessWidget {
-  final Widget child;
-  const AppFrame({super.key, required this.child});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        bottom: const PreferredSize(preferredSize: Size.fromHeight(0), child: _HeaderNav()),
-      ),
-      body: child,
-    );
-  }
-}
-
-class _HeaderNav extends StatelessWidget {
-  const _HeaderNav();
-
-  @override
-  Widget build(BuildContext context) {
-    final loc = GoRouterState.of(context).matchedLocation;
-
-    return Container(
-      alignment: Alignment.centerLeft,
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Row(
-        children: [
-          _NavItem(label: 'Dashboard', to: Routes.dashboard, selected: loc.startsWith(Routes.dashboard)),
-          _NavItem(label: 'Model Manage', to: Routes.modelManage, selected: loc.startsWith(Routes.modelManage)),
-          _NavItem(label: 'Model Log', to: Routes.modelLog, selected: loc.startsWith(Routes.modelLog)),
-          _NavItem(label: 'Server Log', to: Routes.serverLog, selected: loc.startsWith(Routes.serverLog)),
-        ],
-      ),
-    );
-  }
-}
-
-class _NavItem extends StatelessWidget {
-  final String label;
-  final String to;
-  final bool selected;
-
-  const _NavItem({required this.label, required this.to, required this.selected});
-
-  @override
-  Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme;
-    return InkWell(
-      onTap: () => context.go(to),
-      borderRadius: BorderRadius.circular(6),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-        margin: const EdgeInsets.only(right: 8),
-        decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: selected ? color.primary : Colors.transparent, width: 3)),
-        ),
-        child: Text(
-          label,
-          style: TextStyle(
-            fontWeight: selected ? FontWeight.w700 : FontWeight.w500,
-            color: selected ? color.primary : color.onSurfaceVariant,
-          ),
-        ),
-      ),
-    );
-  }
 }
 
 // 실직적인 Router를 담당하는 인스턴스
