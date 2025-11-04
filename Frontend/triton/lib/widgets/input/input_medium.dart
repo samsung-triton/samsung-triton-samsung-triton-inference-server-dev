5c7374539494dbfd60bd2f6088b739321ce51f34
KDU <54763635+kimdonguk822@users.noreply.github.com>
2025-11-04 09:59:11 +0900

    [S13P31S302-51] Feat: InputMedium 컴포넌트 구현

--- Frontend/triton/lib/widgets/input/input_medium.dart
+++ Frontend/triton/lib/widgets/input/input_medium.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import '../../theme/app_colors.dart';
 import '../../theme/typography.dart';
+import 'package:flutter/services.dart'; // ← 중요: MaxLengthEnforcement 사용
 
-class InputMedium extends StatelessWidget {
+class InputMedium extends StatefulWidget {
   final String hintText;
   final TextEditingController? controller;
   final bool enabled;
   final bool hasError;
   final double width;
-  final double height;
   final double borderRadius;
   final EdgeInsets padding;
-  final VoidCallback? onTap;
+  final int maxLength;
 
   const InputMedium({
     Key? key,
@@ -20,42 +20,65 @@ class InputMedium extends StatelessWidget {
     this.enabled = true,
     this.hasError = false,
     this.width = 236,
-    this.height = 24, // Figma height 24보다 살짝 여유
     this.borderRadius = 4,
-    this.padding = const EdgeInsets.symmetric(horizontal: 12),
-    this.onTap,
+    this.padding = const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+    this.maxLength = 200,
   }) : super(key: key);
 
+  @override
+  State<InputMedium> createState() => _InputMediumState();
+}
+
+class _InputMediumState extends State<InputMedium> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) _controller.dispose();
+    super.dispose();
+  }
+
   Color _borderColor() {
-    if (!enabled) return lightGray;
-    if (hasError) return statusRed;
+    if (!widget.enabled) return lightGray;
+    if (widget.hasError) return statusRed;
     return lightGray;
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: width,
-      height: height,
-      alignment: Alignment.centerLeft,
+      width: widget.width,
+      alignment: Alignment.topLeft,
       decoration: BoxDecoration(
         color: white,
-        borderRadius: BorderRadius.circular(borderRadius),
+        borderRadius: BorderRadius.circular(widget.borderRadius),
         border: Border.all(color: _borderColor(), width: 1),
       ),
-      padding: padding,
+      padding: widget.padding,
       child: TextField(
-        controller: controller,
-        enabled: enabled,
-        onTap: onTap,
-        style: T.t12(color: darkGray, bold: false),
+        controller: _controller,
+        enabled: widget.enabled,
+        keyboardType: TextInputType.multiline,
+        textInputAction: TextInputAction.newline,
+        minLines: 1,
+        maxLines: 5,
+        maxLength: widget.maxLength,
+        maxLengthEnforcement: MaxLengthEnforcement.enforced, // 초과 입력 금지
+        style: T.t12(color: darkGray),
         cursorColor: primaryNormal,
         decoration: InputDecoration(
-          isCollapsed: true,
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: T.t12(color: gray),
           border: InputBorder.none,
+          isCollapsed: true,
           contentPadding: EdgeInsets.zero,
+          counterText: '',
         ),
       ),
     );
