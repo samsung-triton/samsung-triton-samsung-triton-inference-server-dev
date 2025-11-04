15c5f98c6e67c63f27d75178586aa1c25def0fbd
ji_eu_nn <najieun5317@naver.com>
2025-11-04 21:40:25 +0900

    [S13P31S302-49] Fix: 타이머 카운트 될 때 화면 갱신 안되는 문제 해결 (bottom -> flexibleSpace)

--- Frontend/triton/lib/app_frame.dart
+++ Frontend/triton/lib/app_frame.dart
@@ -1,12 +1,12 @@
 // 헤더 프레임
 // 공통 상단 위젯을 모든 페이지에서 띄우기 위한 프레임
 import 'package:flutter/material.dart';
+import 'package:triton/widgets/button/button_medium.dart';
 
 import 'package:triton/widgets/header/header_nav.dart';
 import 'package:triton/widgets/header/server_status.dart';
 import 'package:triton/widgets/header/server_control.dart';
 import 'package:triton/widgets/header/name_text.dart';
-import 'package:triton/widgets/button/button_small.dart';
 import 'theme/app_colors.dart';
 
 class AppFrame extends StatelessWidget {
@@ -21,33 +21,29 @@ class AppFrame extends StatelessWidget {
         backgroundColor: white,
         surfaceTintColor: Colors.transparent, //그림자 적용 시, 저절로 톤다운 되는것 막기 위함
         toolbarHeight: 48,
-        bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(48),
-          child: Container(
-            height: 48,
-            padding: const EdgeInsets.symmetric(horizontal: 16).copyWith(right: 32), //수평 padding 주면서 오른쪽 값은 덮어쓰기
+        flexibleSpace: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8).copyWith(right: 32),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                // ---------------- LEFT: 메뉴바 ----------------
                 const HeaderNav(),
-
-                // ---------------- RIGHT: 서버상태 + 제어 + 유저정보 ----------------
                 Row(
                   children: [
-                    const ServerStatus(status: "stopped"),
+                    ServerStatus(status: "running", startTime: DateTime.now().subtract(const Duration(seconds: 25))),
                     const SizedBox(width: 16),
                     ServerControl(
+                      //모달 연결
                       onStart: () => print("서버 시작"),
                       onStop: () => print("서버 중지"),
                       onRestart: () => print("서버 재시작"),
                     ),
                     const SizedBox(width: 16),
-                    Container(width: 2, height: 24, color: black), // 구분선
+                    Container(width: 2, height: 24, color: black),
                     const SizedBox(width: 16),
                     const NameText(name: "tester1"),
                     const SizedBox(width: 8),
-                    const ButtonSmall(text: "Log Out", backgroundColor: black, textColor: white),
+                    const ButtonMedium(text: "Log Out", backgroundColor: black, textColor: white),
                   ],
                 ),
               ],
@@ -57,7 +53,6 @@ class AppFrame extends StatelessWidget {
         elevation: 4,
         shadowColor: black.withValues(alpha: 0.2),
       ),
-      body: child,
     );
   }
 }
