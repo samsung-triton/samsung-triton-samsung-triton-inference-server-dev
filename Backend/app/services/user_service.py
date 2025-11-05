b0937bf7885a694abaabadcc5280dac2ad632ee2
minju <tisu79@naver.com>
2025-11-05 17:30:28 +0900

    chore : newLine 추가

--- Backend/app/services/user_service.py
+++ Backend/app/services/user_service.py
@@ -1,32 +1,22 @@
 from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.base_schema import BaseResponse
-from fastapi import HTTPException, status
+from app.core.response_utils import raise_http_exception, create_response
+from app.constants import CustomCode, Messages
+from fastapi import status
 
 
 def login_user_service(login_id: str, password: str, db: Session) -> BaseResponse:
     if not login_id or not password:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail={"code": "ERR-400", "message": "요청 파라미터가 올바르지 않습니다.", "data": None},
-        )
+        raise_http_exception(status.HTTP_400_BAD_REQUEST, CustomCode.ERR_400.value, Messages.INVALID_PARAM.value)
 
     user = db.query(User).filter(User.login_id == login_id).first()
-
     if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail={"code": "ERR-404", "message": "해당 유저를 찾을 수 없습니다.", "data": None},
-        )
+        raise_http_exception(status.HTTP_404_NOT_FOUND, CustomCode.ERR_404.value, Messages.USER_NOT_FOUND.value)
 
     if user.password != password:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail={"code": "ERR-401", "message": "아이디 또는 비밀번호가 일치하지 않습니다.", "data": None},
-        )
+        raise_http_exception(status.HTTP_401_UNAUTHORIZED, CustomCode.ERR_401.value, Messages.INVALID_CREDENTIAL.value)
 
-    return BaseResponse(
-        code="AUTH-002",
-        message=f"{user.name}님, 로그인이 성공적으로 완료되었습니다.",
-        data={"user_id": user.user_id, "role": user.role},
+    return create_response(
+        CustomCode.AUTH_001.value, Messages.LOGIN_SUCCESS.value, {"user_id": user.user_id, "role": user.role}
     )
