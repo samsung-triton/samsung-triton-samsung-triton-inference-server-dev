da3b4ba7fcf4ee67ed65607315e579ec25e579cc
ji_eu_nn <najieun5317@naver.com>
2025-11-05 17:26:20 +0900

    [S13P31S302-55] Feat: Modellogscreen에 FilterBlock 위젯 배치

--- Frontend/triton/lib/screen/model_log/model_log_screen.dart
+++ Frontend/triton/lib/screen/model_log/model_log_screen.dart
@@ -1,5 +1,6 @@
 // 모델 로그 화면 위젯
 import 'package:flutter/material.dart';
+import 'package:triton/widgets/modellog/filter_block.dart';
 
 class ModelLogScreen extends StatefulWidget {
   const ModelLogScreen({super.key});
@@ -11,6 +12,20 @@ class ModelLogScreen extends StatefulWidget {
 class _ModelLogScreenState extends State<ModelLogScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Model Log"));
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              FilterBlock(),
+              SizedBox(height: 16),
+              Padding(padding: EdgeInsets.symmetric(horizontal: 16), child: Text("Model Log")),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
