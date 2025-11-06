849cef455b539e4ded67bda6d8029d5288ce0b8f
ji_eu_nn <najieun5317@naver.com>
2025-11-06 10:53:59 +0900

    [S13P31S302-55] Design: FilterBlock margin 수정

--- Frontend/triton/lib/widgets/modellog/filter_block.dart
+++ Frontend/triton/lib/widgets/modellog/filter_block.dart
@@ -13,7 +13,7 @@ class FilterBlock extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(8),
-      margin: EdgeInsets.all(8),
+      margin: EdgeInsets.all(4),
       decoration: BoxDecoration(
         color: white,
         borderRadius: BorderRadius.circular(8),
