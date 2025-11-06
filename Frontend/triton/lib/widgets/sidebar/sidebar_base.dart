c74bc80b3ef0b4849ece35a44a720060bef7ffb4
김성민 <skattr13@naver.com>
2025-11-06 17:37:50 +0900

    [S13P31S302-54] Feat: 사이드바 베이스 제작

---
+++ Frontend/triton/lib/widgets/sidebar/sidebar_base.dart
@@ -0,0 +1,36 @@
+// 사이드바 베이스
+import 'package:flutter/material.dart';
+import '../../theme/app_colors.dart';
+
+class SidebarBase extends StatelessWidget {
+  final List<Widget> children;
+
+  // 아이템 사이 간격
+  final double gap = 16;
+
+  const SidebarBase({super.key, required this.children});
+
+  @override
+  Widget build(BuildContext context) {
+    // children 사이 gap 추가
+    final content = <Widget>[];
+    for (int i = 0; i < children.length; i++) {
+      content.add(children[i]);
+      if (i != children.length - 1) {
+        content.add(SizedBox(height: gap));
+      }
+    }
+
+    return Container(
+      width: 300,
+      color: primaryLightest,
+      child: Scrollbar(
+        thumbVisibility: true,
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: content),
+        ),
+      ),
+    );
+  }
+}
