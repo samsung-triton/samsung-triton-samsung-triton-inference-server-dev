8b6df33cb3a3268f5a3ab1249a9561cfb8157820
minjae <robot703@naver.com>
2025-11-08 19:04:28 +0900

    [S13P31S302-61] Fix: merge conflict 해결

--- Backend/app/api/v1/router.py
+++ Backend/app/api/v1/router.py
@@ -1,12 +1,11 @@
 from fastapi import APIRouter
-
 from app.api.v1.user_router import user_router
-
 from app.api.v1.masterkey_router import mastekey_router
-
-from app.api.v1.server_router import server_router
+from app.api.v1.inferdata_router import inferdata_router
+from app.api.v1.server_router import server_router   
 
 api_router = APIRouter()
 api_router.include_router(user_router)
 api_router.include_router(mastekey_router)
-api_router.include_router(server_router)
+api_router.include_router(inferdata_router)
+api_router.include_router(server_router)  
