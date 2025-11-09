4d667b931e09ce27efa6d5030bccb52a3b6f9a72
ji_eu_nn <najieun5317@naver.com>
2025-11-09 15:16:59 +0900

    [S13P31S302-112] Feat: 로그인 된 사용자의 name 연결

--- Frontend/triton/lib/widgets/header/name_text.dart
+++ Frontend/triton/lib/widgets/header/name_text.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
 import '../../theme/typography.dart';
 import '../../theme/app_colors.dart';
+import 'package:get/get.dart';
+import '../../controller/auth/auth_controller.dart';
 
 class NameText extends StatelessWidget {
-  final String name;
-
-  const NameText({super.key, required this.name});
+  final double width;
+  const NameText({super.key, this.width = 100});
 
   @override
   Widget build(BuildContext context) {
-    final displayName = name;
+    final auth = Get.find<AuthController>();
 
-    return Text(displayName, style: T.t20(color: black, bold: false));
+    return SizedBox(
+      width: width,
+      child: Obx(() {
+        final displayName = auth.userName.value.isNotEmpty ? auth.userName.value : 'Guest'; //이름 없으면 guest
+
+        return FittedBox(
+          fit: BoxFit.scaleDown, // 텍스트가 넘칠 때 자동 축소
+          alignment: Alignment.center,
+          child: Text(
+            displayName,
+            style: T.t20(color: black, bold: false),
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis, // 너무 길면 ... 표시
+          ),
+        );
+      }),
+    );
   }
 }
