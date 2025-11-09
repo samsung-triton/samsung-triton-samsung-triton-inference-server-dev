c9ff9c8a7d8ec154689c2c4b7b96009694aaa303
KDU <54763635+kimdonguk822@users.noreply.github.com>
2025-11-09 18:43:19 +0900

    [S13P31S302-53] Feat: Dashboard screen 기능 추가
    - 서버/모델 선택 시 panel 선택 기능 추가

--- Frontend/triton/lib/screen/dashboard/dashboard_screen.dart
+++ Frontend/triton/lib/screen/dashboard/dashboard_screen.dart
@@ -1,16 +1,35 @@
-// 대시보드 화면 위젯
+// lib/screen/dashboard/dashboard_screen.dart
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:triton/controller/dashboard/dashboard_controller.dart';
+import 'package:triton/screen/dashboard/dashboard_sidebar.dart';
+import 'package:triton/widgets/dashboard/model_dashboard_panel.dart';
+import 'package:triton/widgets/dashboard/server_dashboard_panel.dart';
 
-class DashboardScreen extends StatefulWidget {
+class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
-  State<DashboardScreen> createState() => _DashboardScreenState();
-}
-
-class _DashboardScreenState extends State<DashboardScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Dashboard"));
+    final dashboardController = Get.put(DashboardController());
+
+    return Scaffold(
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const DashboardSidebar(),
+          Expanded(
+            child: Obx(() {
+              final selected = dashboardController.selectedMenu.value;
+              if (selected == 'server') {
+                return const ServerDashboardPanel();
+              } else {
+                return const ModelDashboardPanel();
+              }
+            }),
+          ),
+        ],
+      ),
+    );
   }
 }
