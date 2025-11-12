ece9ddbbb774d68a5170b2f0a5c2779dcba5fd7c
minju <tisu79@naver.com>
2025-11-12 10:59:05 +0900

    fix : 요청 DTO 수정

--- Backend/app/schemas/inferoutput_schema.py
+++ Backend/app/schemas/inferoutput_schema.py
@@ -2,5 +2,6 @@ from pydantic import BaseModel, Field
 
 
 class SaveInferenceResultRequest(BaseModel):
-    uid: str = Field(..., min_length=1, description="사용자가 입력한 마스터키")
+    uid: str = Field(..., min_length=1, description="추론 요청 고유 ID")
+    is_ok: bool = Field(..., description="모델 추론 결과 (True=OK, False=NG)")
     result: str = Field(..., min_length=1, description="추론 결과")
