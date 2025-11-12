89086d1b6cc09c609a2e6d085c2e36fb8f28f598
ji_eu_nn <najieun5317@naver.com>
2025-11-12 12:13:50 +0900

    [S13P31S302-127] Feat: FilterControllerServer 연동, 페이지 별로 컨트롤러 주입

--- Frontend/triton/lib/screen/server_log/server_log_screen.dart
+++ Frontend/triton/lib/screen/server_log/server_log_screen.dart
@@ -1,5 +1,7 @@
 // 서버 로그 화면 위젯
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:triton/controller/server_log/filter_controller_server.dart';
 import 'package:triton/widgets/serverlog/server_log_table.dart';
 import 'package:triton/widgets/serverlog/serverlog_header.dart';
 
@@ -11,6 +13,36 @@ class ServerLogScreen extends StatefulWidget {
 }
 
 class _ServerLogScreenState extends State<ServerLogScreen> {
+  late final FilterControllerServer filterControllerServer;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // 페이지 단위로 컨트롤러 주입
+    filterControllerServer = Get.put(FilterControllerServer(), permanent: false);
+  }
+
+  @override
+  void dispose() {
+    // 페이지 나갈 때 컨트롤러 메모리 해제
+    Get.delete<FilterControllerServer>();
+    super.dispose();
+  }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   if (!Get.isRegistered<FilterControllerServer>()) {
+  //     //한번 등록된 컨트롤러는 재사용됨
+  //     Get.put(FilterControllerServer());
+  //   }
+
+  //   // 페이지 진입 시 필터 초기화
+  //   final controller = Get.find<FilterControllerServer>();
+  //   controller.resetFilter();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
