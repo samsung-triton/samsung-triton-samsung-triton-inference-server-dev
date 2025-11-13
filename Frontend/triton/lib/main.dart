908510fee102aaded0c6a154ccc4dadf11723641
김성민 <skattr13@naver.com>
2025-11-13 14:17:30 +0900

    [S13P31S302-112] Feat: 프로젝트에 auth controller와 storage 연결

--- Frontend/triton/lib/main.dart
+++ Frontend/triton/lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'router.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:triton/router.dart';
 import 'package:triton/utils/api_client.dart';
+import 'package:triton/controller/auth/auth_controller.dart';
+
 import './theme/app_colors.dart';
 
-void main() {
+void main() async {
+  await GetStorage.init('auth');
+
   Get.put<ApiClient>(ApiClient(), permanent: true);
+  Get.put<AuthController>(AuthController(), permanent: true);
   runApp(const MyApp());
 }
 
