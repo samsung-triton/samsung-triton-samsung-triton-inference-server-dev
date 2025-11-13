16cd10a26d35b7958897b5f180ea84b4d508c689
김성민 <skattr13@naver.com>
2025-11-13 14:20:03 +0900

    [S13P31S302-112] Refactor: controller 의존성 storage 이동

--- Frontend/triton/lib/widgets/header/name_text.dart
+++ Frontend/triton/lib/widgets/header/name_text.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+
 import '../../theme/typography.dart';
 import '../../theme/app_colors.dart';
-import 'package:get/get.dart';
-import '../../controller/auth/auth_controller.dart';
 
 class NameText extends StatelessWidget {
   final double width;
   const NameText({super.key, this.width = 100});
 
+  GetStorage get _authStorage => GetStorage('auth');
+
   @override
   Widget build(BuildContext context) {
-    final auth = Get.find<AuthController>();
+    final displayName = _authStorage.read<String>('loginedId') ?? '';
 
     return SizedBox(
       width: width,
-      child: Obx(() {
-        final displayName = auth.userName.value.isNotEmpty ? auth.userName.value : 'Guest'; //이름 없으면 guest
-
-        return FittedBox(
-          fit: BoxFit.scaleDown, // 텍스트가 넘칠 때 자동 축소
-          alignment: Alignment.center,
-          child: Text(
-            displayName,
-            style: T.t20(color: black, bold: false),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis, // 너무 길면 ... 표시
-          ),
-        );
-      }),
+      child: FittedBox(
+        fit: BoxFit.scaleDown, // 텍스트가 넘칠 때 자동 축소
+        alignment: Alignment.center,
+        child: Text(
+          displayName,
+          style: T.t20(color: black, bold: false),
+          maxLines: 1,
+          overflow: TextOverflow.ellipsis, // 너무 길면 ... 표시
+        ),
+      ),
     );
   }
 }
