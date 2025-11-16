aa220214b6952e4cf09f64929632dfe094f4eadb
minjae <robot703@naver.com>
2025-11-16 16:05:40 +0900

    [S13P31S302-133] Fix: cors에러 수정

--- Backend/main.py
+++ Backend/main.py
@@ -23,7 +23,7 @@ def create_app() -> FastAPI:
         CORSMiddleware,
         allow_origins=["*"],
         allow_credentials=True,
-        allow_methods=["*"],
+        allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
         allow_headers=["*"],
     )
 
