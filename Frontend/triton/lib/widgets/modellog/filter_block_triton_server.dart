c4f2aab46f383219eb5501a19169cc9930867a5a
ji_eu_nn <najieun5317@naver.com>
2025-11-19 21:30:01 +0900

    [S13P31S302-165] Rename: 파일명 변경 (FilterBlockTritonServer)

--- Frontend/triton/lib/widgets/modellog/filter_block_triton_server.dart
+++ Frontend/triton/lib/widgets/modellog/filter_block_triton_server.dart
@@ -8,14 +8,14 @@ import 'package:triton/widgets/input/dropdown.dart';
 import 'package:triton/widgets/modellog/filter_text.dart';
 import 'package:triton/widgets/modellog/MiniDatePicker.dart';
 
-class FilterBlockTriton extends StatefulWidget {
-  const FilterBlockTriton({super.key});
+class FilterBlockTritonServer extends StatefulWidget {
+  const FilterBlockTritonServer({super.key});
 
   @override
-  State<FilterBlockTriton> createState() => _FilterBlockTritonState();
+  State<FilterBlockTritonServer> createState() => _FilterBlockTritonServerState();
 }
 
-class _FilterBlockTritonState extends State<FilterBlockTriton> {
+class _FilterBlockTritonServerState extends State<FilterBlockTritonServer> {
   final keywordCtrl = TextEditingController();
 
   @override
