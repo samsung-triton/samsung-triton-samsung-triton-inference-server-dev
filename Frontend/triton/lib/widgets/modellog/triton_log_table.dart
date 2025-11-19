ba201d36da3ecdb03a30d4e3affc5eb225aed292
ji_eu_nn <najieun5317@naver.com>
2025-11-19 10:57:34 +0900

    [S13P31S302-160] Feat: TrironLogTable 컨트롤러 연결, 무한스크롤 적용

--- Frontend/triton/lib/widgets/modellog/triton_log_table.dart
+++ Frontend/triton/lib/widgets/modellog/triton_log_table.dart
@@ -1,67 +1,109 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
+import 'package:triton/controller/model_log/model_log_controller.dart';
+import 'package:triton/controller/model_log/triton_infer_log_controller.dart';
+import 'package:triton/controller/model_log/triton_log_controller.dart';
 import 'package:triton/theme/app_colors.dart';
 import 'package:triton/theme/typography.dart';
+import 'package:triton/widgets/modellog/infer_log_table_row.dart';
 import 'package:triton/widgets/modellog/triton_log_table_header.dart';
 import 'package:triton/widgets/modellog/triton_log_table_row.dart';
-import 'package:triton/controller/model_log/model_log_controller.dart';
 
-class TritonLogTable extends StatelessWidget {
+class TritonLogTable extends StatefulWidget {
   const TritonLogTable({super.key});
 
   @override
+  State<TritonLogTable> createState() => _TritonLogTableState();
+}
+
+class _TritonLogTableState extends State<TritonLogTable> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      final tritonController = Get.find<TritonLogController>();
+      final modelController = Get.find<ModelLogController>();
+
+      final tritonInferController = Get.find<TritonInferLogController>();
+
+      if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 500) {
+        final model = tritonInferController.modelName.value;
+
+        if (model == 'triton') {
+          tritonController.fetchMoreLogs();
+        } else {
+          modelController.fetchMoreLogs();
+        }
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final controller = Get.find<ModelLogController>();
+    final tritonController = Get.find<TritonLogController>();
+    final modelController = Get.find<ModelLogController>();
+
+    final tritonInferController = Get.find<TritonInferLogController>();
 
     return Container(
-      //margin: EdgeInsets.symmetric(vertical: 2),
       padding: const EdgeInsets.all(8),
       color: white,
       child: Column(
         children: [
           const TritonLogTableHeader(),
+
           Expanded(
             child: Obx(() {
-              // 모델 선택 안한 경우
-              if (controller.modelName.value.isEmpty) {
+              final model = tritonInferController.modelName.value;
+
+              if (model.isEmpty) {
                 return Center(
-                  child: Text('Please select a model.', style: T.t12(color: gray, bold: false)),
+                  child: Text('Please select a model.', style: T.t12(color: gray)),
                 );
               }
 
-              final logs = controller.filteredLogs; // 필터링된 로그 가져오기
+              // 🔥 로그 분기
+              final logs = (model == 'triton') ? tritonController.filteredLogs : modelController.filteredLogs;
 
-              // 필터링 결과가 없는 경우
               if (logs.isEmpty) {
                 return Center(
-                  child: Text(
-                    'There are no logs matching the selected conditions.',
-                    style: T.t12(color: gray, bold: false),
-                  ),
+                  child: Text('There are no logs matching the selected conditions.', style: T.t12(color: gray)),
                 );
               }
 
               return RawScrollbar(
                 thumbColor: lightGray,
                 radius: const Radius.circular(4),
-                thickness: 8, // 두께
-                thumbVisibility: false, // 항상 보이게
-                interactive: true, // 드래그로 스크롤 가능
-                child: ListView.builder(
-                  itemCount: logs.length,
-                  itemBuilder: (context, i) {
-                    final log = logs[i];
-                    return TritonLogTableRow(
-                      log: {
-                        // 기존 구조에 맞게 변환
-                        'date': DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.parse(log['date'])), //출력 파싱
-                        'level': log['level'],
-                        'detail': log['message'],
+                thickness: 8,
+                interactive: true,
+                child: Obx(() {
+                  final model = tritonInferController.modelName.value;
+
+                  if (model == 'triton') {
+                    final logs = tritonController.filteredLogs;
+
+                    return ListView.builder(
+                      controller: scrollController,
+                      itemCount: logs.length,
+                      itemBuilder: (context, i) {
+                        return TritonLogTableRow(log: logs[i]);
                       },
                     );
-                  },
-                ),
+                  } else {
+                    final logs = modelController.filteredLogs;
+
+                    return ListView.builder(
+                      controller: scrollController,
+                      itemCount: logs.length,
+                      itemBuilder: (context, i) {
+                        return InferLogTableRow(log: logs[i]);
+                      },
+                    );
+                  }
+                }),
               );
             }),
           ),
