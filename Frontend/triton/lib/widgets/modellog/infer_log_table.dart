89b9b4ee9b45e0ac1baa1ab99fea90149ec459cf
ji_eu_nn <najieun5317@naver.com>
2025-11-19 10:56:21 +0900

    [S13P31S302-160] Feat: InferLogTable 구현

---
+++ Frontend/triton/lib/widgets/modellog/infer_log_table.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:triton/controller/model_log/model_log_controller.dart';
+import 'package:triton/controller/model_log/triton_infer_log_controller.dart';
+import 'package:triton/controller/model_log/triton_log_controller.dart';
+import 'package:triton/theme/app_colors.dart';
+import 'package:triton/theme/typography.dart';
+import 'package:triton/widgets/modellog/infer_log_table_row.dart';
+import 'package:triton/widgets/modellog/triton_log_table_header.dart';
+
+class InferLogTable extends StatefulWidget {
+  const InferLogTable({super.key});
+
+  @override
+  State<InferLogTable> createState() => _InferLogTableState();
+}
+
+class _InferLogTableState extends State<InferLogTable> {
+  final ScrollController scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    scrollController.addListener(() {
+      final modelController = Get.find<TritonLogController>();
+
+      if (scrollController.position.pixels >= scrollController.position.maxScrollExtent - 500) {
+        modelController.fetchMoreLogs(); // 무한 스크롤 트리거
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final modelController = Get.find<ModelLogController>();
+    final tritonInferController = Get.find<TritonInferLogController>();
+
+    return Container(
+      padding: const EdgeInsets.all(8),
+      color: white,
+      child: Column(
+        children: [
+          const TritonLogTableHeader(),
+          Expanded(
+            child: Obx(() {
+              if (tritonInferController.modelName.value.isEmpty) {
+                return Center(
+                  child: Text('Please select a model.', style: T.t12(color: gray, bold: false)),
+                );
+              }
+
+              final logs = modelController.filteredLogs;
+
+              if (logs.isEmpty) {
+                return Center(
+                  child: Text(
+                    'There are no logs matching the selected conditions.',
+                    style: T.t12(color: gray, bold: false),
+                  ),
+                );
+              }
+
+              return RawScrollbar(
+                thumbColor: lightGray,
+                radius: const Radius.circular(4),
+                thickness: 8,
+                interactive: true,
+                child: ListView.builder(
+                  controller: scrollController, // 연결
+                  itemCount: logs.length,
+                  itemBuilder: (context, i) {
+                    final log = logs[i];
+                    return InferLogTableRow(log: log);
+                  },
+                ),
+              );
+            }),
+          ),
+        ],
+      ),
+    );
+  }
+}
