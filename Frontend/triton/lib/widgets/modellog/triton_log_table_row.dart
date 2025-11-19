a87f0ead773f751d092ec2f7dce956e25763a085
ji_eu_nn <najieun5317@naver.com>
2025-11-19 10:57:01 +0900

    [S13P31S302-160] Feat: TrironLogTableRow 컨트롤러 연결

--- Frontend/triton/lib/widgets/modellog/triton_log_table_row.dart
+++ Frontend/triton/lib/widgets/modellog/triton_log_table_row.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:triton/controller/model_log/triton_log_controller.dart';
 import 'package:triton/theme/app_colors.dart';
 import 'package:triton/theme/typography.dart';
 
 class TritonLogTableRow extends StatelessWidget {
-  final Map<String, String> log;
+  final TritonLogItem log;
+
   const TritonLogTableRow({super.key, required this.log});
 
   @override
@@ -16,9 +18,9 @@ class TritonLogTableRow extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start, // 여러 줄일 때 위로 정렬
         children: [
-          _textCell(log['date']!, width: 120),
-          _textCell(log['level']!, width: 120),
-          _textCell(log['detail']!, expanded: true, textAlign: TextAlign.left),
+          _textCell(log.ts, width: 120),
+          _textCell(log.level, width: 120),
+          _textCell(log.message, expanded: true, textAlign: TextAlign.left),
         ],
       ),
     );
