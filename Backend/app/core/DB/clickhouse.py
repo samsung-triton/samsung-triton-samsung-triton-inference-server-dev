4961c162c4be67e3a3aecf6d9cadb6df722e76fe
minju <tisu79@naver.com>
2025-11-20 14:04:08 +0900

    서버 메트릭 실시간 통신으로 변경

--- Backend/app/core/DB/clickhouse.py
+++ Backend/app/core/DB/clickhouse.py
@@ -8,8 +8,7 @@ from app.core.config import settings
 #     f"@{settings.CHK_HOST}:{settings.CHK_PORT}/{settings.CHK_DB}"
 # )
 CLICKHOUSE_URL = (
-    f"clickhouse+http://{settings.CHK_USER}:{settings.CHK_PASSWORD}"
-    f"@{settings.CHK_HOST}:{settings.CHK_PORT}/{settings.CHK_DB}"
+    f"clickhouse+native://{settings.CHK_USER}:{settings.CHK_PASSWORD}" f"@{settings.CHK_HOST}:9000/{settings.CHK_DB}"
 )
 
 ch_engine = create_engine(CLICKHOUSE_URL)
