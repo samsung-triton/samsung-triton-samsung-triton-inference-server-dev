e01ee6fb5f65b73aa3aa8cbfefaf19386cf41983
seong min Kim <52230142+lkkikkil@users.noreply.github.com>
2025-11-26 12:50:27 +0900

    Comment: 로그인 관련 주석 수정

--- Frontend/triton/lib/widgets/login/login_text_field.dart
+++ Frontend/triton/lib/widgets/login/login_text_field.dart
@@ -1,7 +1,7 @@
 // 로그인 텍스트 입력칸 (ID, PW)
 import 'package:flutter/material.dart';
-import '../../theme/app_colors.dart';
-import '../../theme/typography.dart';
+import 'package:triton/theme/app_colors.dart';
+import 'package:triton/theme/typography.dart';
 
 class LoginTextField extends StatefulWidget {
   final String label;
@@ -12,7 +12,7 @@ class LoginTextField extends StatefulWidget {
   final TextInputAction? textInputAction;
   final ValueChanged<String>? onSubmitted;
 
-  // 비밀번호 관련 세팅
+  // 비밀번호 입력 확인 속성
   final bool isPassword;
   final bool initialObscure;
 
@@ -37,7 +37,6 @@ class _LoginTextFieldState extends State<LoginTextField> {
   late final TextEditingController _controller;
   late bool _obscure;
 
-  // 초기화
   @override
   void initState() {
     super.initState();
@@ -45,72 +44,66 @@ class _LoginTextFieldState extends State<LoginTextField> {
     _obscure = widget.isPassword ? widget.initialObscure : false;
   }
 
-  // 삭제
   @override
   void dispose() {
     if (widget.controller == null) _controller.dispose();
     super.dispose();
   }
 
-  // 생성 빌드
   @override
   Widget build(BuildContext context) {
-    // 텍스트 필드
-    final textField = Container(
-      width: double.infinity,
-      height: 64,
-      alignment: Alignment.centerLeft,
-      decoration: BoxDecoration(color: lightGray, borderRadius: BorderRadius.circular(widget.borderRadius)),
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: TextField(
-        controller: _controller,
-        textInputAction: widget.textInputAction,
-        onSubmitted: widget.onSubmitted,
-
-        // 중앙정렬
-        textAlignVertical: TextAlignVertical.center,
-
-        // PW일 때만 마스킹
-        obscureText: widget.isPassword ? _obscure : false,
-
-        keyboardType: TextInputType.text,
-        maxLines: 1,
-
-        style: T.t20(color: darkGray),
-
-        decoration: InputDecoration(
-          hintText: widget.hintText,
-          hintStyle: T.t20(color: gray),
-          border: InputBorder.none,
-          isCollapsed: true,
-          contentPadding: EdgeInsets.zero,
-          counterText: '',
-
-          // 비밀번호 보임/안보인 아이콘
-          suffixIcon: widget.isPassword
-              ? SizedBox(
-                  height: 40,
-                  child: IconButton(
-                    onPressed: () => setState(() => _obscure = !_obscure),
-                    icon: Icon(_obscure ? Icons.visibility_off : Icons.visibility, color: darkGray, size: 24),
-                  ),
-                )
-              : const SizedBox(height: 40, width: 40),
-        ),
-      ),
-    );
-
-    // 최종 위젯 리턴
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        // 라벨이 있으면 라벨 표시
+        // 라벨
         if (widget.label.isNotEmpty)
           Padding(
             padding: const EdgeInsets.only(bottom: 8),
             child: Text(widget.label, style: T.t20(color: darkGray)),
           ),
-        textField,
+
+        // 입력 필드
+        Container(
+          width: double.infinity,
+          height: 64,
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(color: lightGray, borderRadius: BorderRadius.circular(widget.borderRadius)),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            controller: _controller,
+            textInputAction: widget.textInputAction,
+            onSubmitted: widget.onSubmitted,
+            textAlignVertical: TextAlignVertical.center,
+
+            // PW 마스킹
+            obscureText: widget.isPassword ? _obscure : false,
+
+            keyboardType: TextInputType.text,
+            maxLines: 1,
+
+            style: T.t20(color: darkGray),
+
+            decoration: InputDecoration(
+              hintText: widget.hintText,
+              hintStyle: T.t20(color: gray),
+              border: InputBorder.none,
+              isCollapsed: true,
+              contentPadding: EdgeInsets.zero,
+              counterText: '',
+
+              // 비밀번호 보임/안보인 아이콘
+              suffixIcon: widget.isPassword
+                  ? SizedBox(
+                      height: 40,
+                      child: IconButton(
+                        onPressed: () => setState(() => _obscure = !_obscure),
+                        icon: Icon(_obscure ? Icons.visibility_off : Icons.visibility, color: darkGray, size: 24),
+                      ),
+                    )
+                  : const SizedBox(height: 40, width: 40),
+            ),
+          ),
+        ),
       ],
     );
   }
