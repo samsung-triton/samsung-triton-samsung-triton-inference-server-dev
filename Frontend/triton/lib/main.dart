2381f2e09893cb75d72423df6bb7de8ac97cb627
seong min Kim <52230142+lkkikkil@users.noreply.github.com>
2025-11-26 17:24:10 +0900

    Comment: 화면 설정 주석 수정

--- Frontend/triton/lib/main.dart
+++ Frontend/triton/lib/main.dart
@@ -1,15 +1,15 @@
+// 화면 전체 메인
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:triton/router.dart';
+import 'package:triton/theme/app_colors.dart';
 import 'package:triton/utils/api_client.dart';
 import 'package:triton/controller/auth/auth_controller.dart';
 
-import './theme/app_colors.dart';
-
 void main() async {
+  // api + 계정 관련 모든 페이지에서 이용
   await GetStorage.init('auth');
-
   Get.put<ApiClient>(ApiClient(), permanent: true);
   Get.put<AuthController>(AuthController(), permanent: true);
   runApp(const MyApp());
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 화면 라우터 설정
     return MaterialApp.router(
       title: 'Triton Server Monitor',
       debugShowCheckedModeBanner: false,
