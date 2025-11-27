699554225b914ecc89c51a42f17809ad2a147e3d
minjae <robot703@naver.com>
2025-11-27 13:44:29 +0900

    Refactor: 서버 스트림 코드 통일 및 response 수정

--- Backend/app/common/sse_docker.py
+++ Backend/app/common/sse_docker.py
@@ -3,7 +3,7 @@ import asyncio
 from app.core.response_utils import create_response
 from app.common.codes import CustomCode
 from app.common.messages import Messages
-
+from datetime import datetime, timezone
 subscribers = set()
 
 
@@ -40,23 +40,39 @@ def convert_status(action: str):
 def build_response(status: str):
     """create_response로 통일된 응답 포맷 생성"""
 
+    # UTC 시간
+    now_utc = datetime.now(timezone.utc).isoformat()
+
     if status == "start":
         return create_response(
-            code=CustomCode.DOCKER_002.value, message=Messages.SERVER_START_SUCCESS.value, data={"status": "start"}
+            code=CustomCode.DOCKER_002.value,
+            message=Messages.SERVER_START_SUCCESS.value,
+            data={
+                "status": "start",
+                "started_at": now_utc
+            }
         )
 
     if status == "stop":
         return create_response(
-            code=CustomCode.DOCKER_003.value, message=Messages.SERVER_STOP_SUCCESS.value, data={"status": "stop"}
+            code=CustomCode.DOCKER_003.value,
+            message=Messages.SERVER_STOP_SUCCESS.value,
+            data={
+                "status": "stop"
+            }
         )
 
     if status == "restart":
         return create_response(
-            code=CustomCode.DOCKER_004.value, message=Messages.SERVER_RESTART_SUCCESS.value, data={"status": "restart"}
+            code=CustomCode.DOCKER_004.value,
+            message=Messages.SERVER_RESTART_SUCCESS.value,
+            data={
+                "status": "restart",
+                "started_at": now_utc
+            }
         )
 
     return None
-
 
 async def docker_event_watcher(container_name: str):
     client = docker.from_env()
