d88fb9203794a6540842010a95e95274de4bcb13
JIYUN <agy0512@naver.com>
2025-11-27 09:01:14 +0900

    Fix: 클릭하우스 복구

--- Backend/app/core/DB/clickhouse.py
+++ Backend/app/core/DB/clickhouse.py
@@ -3,13 +3,13 @@ from clickhouse_sqlalchemy import make_session
 
 from app.core.config import settings
 
-CLICKHOUSE_URL = (
-    f"clickhouse+http://{settings.CHK_USER}:{settings.CHK_PASSWORD}"
-    f"@{settings.CHK_HOST}:{settings.CHK_PORT}/{settings.CHK_DB}"
-)
 # CLICKHOUSE_URL = (
-#     f"clickhouse+native://{settings.CHK_USER}:{settings.CHK_PASSWORD}@{settings.CHK_HOST}:9000/{settings.CHK_DB}"
+#     f"clickhouse+http://{settings.CHK_USER}:{settings.CHK_PASSWORD}"
+#     f"@{settings.CHK_HOST}:{settings.CHK_PORT}/{settings.CHK_DB}"
 # )
+CLICKHOUSE_URL = (
+    f"clickhouse+native://{settings.CHK_USER}:{settings.CHK_PASSWORD}@{settings.CHK_HOST}:9000/{settings.CHK_DB}"
+)
 
 ch_engine = create_engine(CLICKHOUSE_URL)
 
